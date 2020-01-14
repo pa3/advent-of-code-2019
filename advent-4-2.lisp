d687fa0029fb3a0e39e17e218c23af61d918a24d
Fedor Kulikov <f.kulikov@gmail.com>
2020-01-14 21:01:11 +0100

    add solution for day 4

--- advent-4-2.lisp
+++ advent-4-2.lisp
@@ -4,9 +4,15 @@
     r))
 
 (defun same-digits? (num)
-  (loop for n from 0 to 4
-     thereis (= (nth-digit num n)
-                (nth-digit num (1+ n)))))
+  (loop
+     with seq-length = 1
+     for n from 0 to 4
+     do (progn
+          (unless (= (nth-digit num n)
+                     (nth-digit num (1+ n)))
+            (if (= seq-length 2) (return t)))
+          (setf seq-length 1))
+     finally (if (= seq-length 2) (return t))))
 
 (defun never-decreases? (num)
   (loop for n from 0 to 4
